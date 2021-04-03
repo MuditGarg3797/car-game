b26cd63d91499fddc437e7bd37e2b9c04d347db7
MuditGarg3797 <74240395+MuditGarg3797@users.noreply.github.com>
2021-04-03 15:55:55 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,55 @@
+class Form {
+
+  constructor() {
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.button2 =createButton('CONTROLS')
+    this.greeting = createElement('h2');
+    this.title = createElement('h2');
+    this.reset = createButton('Reset');
+    this.controls= createElement('h1')
+  }
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+    this.title.hide();
+    this.button2.hide();
+  }
+
+  display(){
+    this.title.html("Car Racing Game");
+    this.title.position(displayWidth/2 - 50, 0);
+
+    this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
+    this.button.position(displayWidth/2 + 30, displayHeight/2);
+    this.button2.position(displayWidth/2 + 70, displayHeight/2)
+    this.reset.position(displayWidth-100,20);
+
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
+      player.name = this.input.value();
+      playerCount+=1;
+      player.index = playerCount;
+      player.update();
+      player.updateCount(playerCount);
+      this.greeting.html("Hello " + player.name)
+      this.greeting.position(displayWidth/2 - 70, displayHeight/4);
+    });
+    this.button2.mousePressed(()=>{
+    this.controls.html("UP ARROW - FORWARD"
+    )
+    this.controls.position(displayWidth/2, displayHeight/2)
+                    });
+
+
+    this.reset.mousePressed(()=>{
+      player.updateCount(0);
+      game.update(0);
+      Player.updateCarAtEnd(0);
+
+    });
+
+  }
+}
